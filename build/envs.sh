5eb799a19c8f84e7b81e2f5875e3670cc6a2abfb
Akihiro Harai <mail@harai.click>
2024-05-01 15:33:43 +0900

    Hide or resolve static check warnings

--- build/envs.sh
+++ build/envs.sh
@@ -1,5 +1,9 @@
-export USER_ID="$(id -u)"
-export GROUP_ID="$(id -g)"
+#!/bin/bash
+
+USER_ID="$(id -u)"
+GROUP_ID="$(id -g)"
+export USER_ID
+export GROUP_ID
 export PHP_VERSION="${PHP_VERSION:-7.3}"
 export WP_VERSION="${WP_VERSION:-5.9.4}"
 export WP_MULTISITE="${WP_MULTISITE:-0}"
